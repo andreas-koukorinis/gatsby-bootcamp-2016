4cb06a45cbaf4587ba647897da769f732f403e4c
gbohner <gbohner@gatsby.ucl.ac.uk>
2016-09-19 12:58:26 +0100

    Wrote code to explore structure

--- matlab/monday_1140_1300/part_01_datatypes_ex1.m
+++ matlab/monday_1140_1300/part_01_datatypes_ex1.m
@@ -5,13 +5,36 @@
 % Gergo Bohner
 
 clear
+clc
 % The line below loads a structure S into the workspace
 load('data/part_01_datatypes_ex1')
 
 
 % Explore the structure, learn what kind of data does it represent
 
+all_scores = []; % Create empty array
 
+subjects = {'Cell Biology', 'Machine Learning', 'Building Robots in a week'};
+
+for subjs = 1:3
+  for st_id = 1:max([S.student_id]) % Iterates through all student ids
+    for i1 = 1:length(S) % Iterate through all entries in S
+      % Display the entry if student id is st_id and subject is
+      % subjects{subjs}
+      if (S(i1).student_id == st_id) && strcmp(S(i1).subject, subjects{subjs}) 
+        disp(S(i1))
+        all_scores = [all_scores, S(i1).grade];
+      end
+    end
+  end 
+end
+
+
+avg_score = mean(all_scores);
+
+pass = avg_score > 40;
+
+disp(pass)
 
 % Create new representations of the data, that you find more logical
 
